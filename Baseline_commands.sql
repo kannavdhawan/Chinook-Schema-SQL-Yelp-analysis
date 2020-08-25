71c7ecab98f7e679c2cf7625ac3c19fada026e18
Kannav Dhawan <30934308+kannavdhawan@users.noreply.github.com>
2020-08-25 00:51:06 -0400

    Update Baseline_commands.sql

--- Baseline_commands.sql
+++ Baseline_commands.sql
@@ -4,4 +4,7 @@ SELECT TrackId
 FROM Tracks
 WHERE Milliseconds>=5000000 ;
 
-
+-- Invoices whose total is between $5 and $15 dollars.
+SELECT InvoiceID 
+FROM Invoices
+WHERE Total BETWEEN 5 AND 15;
