25141dbbbfd2a15871f233aef68c22ad280a1ba9
Kannav Dhawan <30934308+kannavdhawan@users.noreply.github.com>
2020-08-25 00:55:57 -0400

    Update Baseline_commands.sql

--- Baseline_commands.sql
+++ Baseline_commands.sql
@@ -20,13 +20,37 @@ WHERE State IN ('RJ','DF','AB', 'BC', 'CA', 'WA', 'NY')
 SELECT * 
 FROM Invoices 
 WHERE (CustomerID=56 OR CustomerID=58) AND Total BETWEEN 1.00 AND 5.00
+;
 
 --  Tracks whose name starts with 'All'.
 SELECT TrackID 
 FROM Tracks
-WHERE Name LIKE 'All%';
+WHERE Name LIKE 'All%'
+;
 
 -- Customer emails that start with "J" and are from gmail.com.
 SELECT * 
 FROM Customers 
 WHERE Email LIKE 'J%@gmail.com'
+;
+
+-- Invoices from the billing city Brasília, Edmonton, and Vancouver and sort in descending order by invoice ID.
+SELECT * 
+FROM Invoices
+WHERE BillingCity IN('Brasília', 'Edmonton','Vancouver')
+ORDER BY InvoiceID DESC
+;
+
+-- Number of orders placed by each customer (hint: this is found in the invoices table) and sort the result by the number of orders in descending order.
+SELECT COUNT(*) AS COUNT,CustomerID 
+FROM Invoices
+GROUP BY CustomerID
+ORDER BY COUNT(*) DESC
+;
+
+-- Albums with 12 or more tracks.
+SELECT *
+FROM Tracks
+GROUP BY AlbumID 
+HAVING COUNT(TrackID)>=12
+;
